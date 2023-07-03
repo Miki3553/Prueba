5f167dc60bd3698b93597dcedcef103abb376b55
Miki3553 <jmjm3553@gmail.com>
2023-07-02 23:21:41 -0500

    Contar A's y E's

---
+++ vocales.py
@@ -0,0 +1,24 @@
+class Vocales(object):
+    def contar_a (self, palabra):
+        a = palabra
+        cont = 0
+        for i in range(0,len(a)):
+            if a[i] == 'a' or a[i] == 'A':
+                cont +=1
+        return cont
+    def contar_e (self, palabra):
+        e = palabra
+        cont = 0
+        for i in range(0,len(e)):
+            if e[i] == 'e' or e[i] == 'E':
+                cont +=1
+        return cont
+    
+
+
+
+
+vocal = Vocales()
+palabra = input()
+print(vocal.contar_a(palabra))
+print(vocal.contar_e(palabra))    
