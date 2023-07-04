d070f57b18156088e28c86940245febd38cdcf91
Miki3553 <jmjm3553@gmail.com>
2023-07-04 00:37:34 -0500

    f1

--- test.py
+++ test.py
@@ -69,27 +69,26 @@ def Game():
         player_map = GeneratePlayerMap(n)
         score = 0
         while True:
-            if CheckWon(player_map) == False:
-                print("Enter your cell you want to open :")
-                x = input("X (1 to 5) :")
-                y = input("Y (1 to 5) :")
-                x = int(x) - 1 # 0 based indexing
-                y = int(y) - 1 # 0 based indexing
-                if (minesweeper_map[y][x] == 'X'):
-                    print("Game Over!")
-                    DisplayMap(minesweeper_map)
-                    GameStatus = CheckContinueGame(score)
-                    break
-                else:
-                    player_map[y][x] = minesweeper_map[y][x]
-                    DisplayMap(player_map)
-                    score += 1
- 
-            else:
-                DisplayMap(player_map)
-                print("You have Won!")
-                GameStatus = CheckContinueGame(score)
-                break
+            while score != (n*n - k):    
+                if CheckWon(player_map) == False:
+                    print("Enter your cell you want to open :")
+                    x = input("X (1 to 5) :")
+                    y = input("Y (1 to 5) :")
+                    x = int(x) - 1 # 0 based indexing
+                    y = int(y) - 1 # 0 based indexing
+                    if (minesweeper_map[y][x] == 'X'):
+                        print("Game Over!")
+                        DisplayMap(minesweeper_map)
+                        GameStatus = CheckContinueGame(score)
+                        break
+                    else:
+                        player_map[y][x] = minesweeper_map[y][x]
+                        DisplayMap(player_map)
+                        score += 1
+    
+            DisplayMap(player_map)
+            print("You have Won!")
+            GameStatus = CheckContinueGame(score)
 # Start of Program
 if __name__ == "__main__":
     try:
